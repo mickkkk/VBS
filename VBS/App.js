9c97b3865e926d14ba6af25f3b2e8d3698ae7a6f
mickkkk <m.vranken@student.fontys.nl>
2018-11-19 16:40:58 +0100

    Added LoginForm with Redux setup

--- VBS/App.js
+++ VBS/App.js
@@ -1,18 +1,38 @@
 import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
+
 import AppNavigator from './navigation/AppNavigator';
+import reducers from './reducers';
+import LoginForm from './components/LoginForm';
 
 const imgRobotDev = require('./assets/images/robot-dev.png');
 const imgRobotProd = require('./assets/images/robot-prod.png');
 const SpaceMonoRegular = require('./assets/fonts/SpaceMono-Regular.ttf');
 
 export default class App extends React.Component {
-  
-  
   state = {
     isLoadingComplete: false
   };
+
+  componentDidMount() {
+    // Initialize Firebase
+    const config = {
+      apiKey: 'AIzaSyAeGaTaMbHfXQ3nUiXiIGtUJAH4OYeHExk',
+      authDomain: 'vastgoedbs-663cd.firebaseapp.com',
+      databaseURL: 'https://vastgoedbs-663cd.firebaseio.com',
+      projectId: 'vastgoedbs-663cd',
+      storageBucket: 'vastgoedbs-663cd.appspot.com',
+      messagingSenderId: '786621546607'
+    };
+    if (firebase.apps.length === 0) {
+      firebase.initializeApp(config);
+    }
+  }
 
   handleFinishLoading = () => {
     this.setState({ isLoadingComplete: true });
@@ -49,11 +69,17 @@ export default class App extends React.Component {
         />
       );
     }
+
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
     return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <AppNavigator />
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+          <LoginForm />
+          {/* <AppNavigator /> */}
+        </View>
+      </Provider>
     );
   }
 }
