55730866cc5960a13fb44027a2f9f9ac8415890e
mickkkk <m.vranken@student.fontys.nl>
2018-11-26 14:56:12 +0100

    Center content Card

--- VBS/components/Card.js
+++ VBS/components/Card.js
@@ -22,7 +22,7 @@ const styles = {
         //elevation: 1,
         marginLeft: 5,
         marginRight: 5,
-        marginTop: 100,
+        marginTop: 0,
         alignItems: 'center',
         justifyContent: 'center',
     }
